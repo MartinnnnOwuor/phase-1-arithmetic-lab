4f9ea8743ed8af88e69efd1f76dfa4f96b946888
martin-XPS-15-9510 <oowuoroowuor@gmail.com>
2024-03-22 15:32:42 +0300

    manipulate arithmetic functions

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
 
 // Write your code here
+
+// first test multiplicity
+
+const num1 = 31;
+const num2 = 2;
+
+let multiply = num1 * num2;
+console.log (multiply);
+
+// second test random integer > 0
+
+let random = 1;
+Number.isInteger(random) !== 0;
+
+// test 3 calculating a rem = 4
+
+let num3 = 14;
+let num4 = 10;
+let mod = 4;
+
+let results = (num3 / num4) 
+console.log(results);
+
+
+// test 4 identifying the highest value in a set
+
+const max = 20;
+Math.max( 5, 10, 15, 17, 18.6, 19.9, 20);
+console.log(max);
+
+
+
+
+
+
+    
